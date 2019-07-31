9e08d821a034fbee647e294854956cad698efef6
Daniel Delgado <danielalejandro.ds@gmail.com>
2019-07-31 18:53:47 +0200

    testing vue

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+    <header-nav :title="title" 
+                :show="show" 
+                @changeShow="changeStateText"></header-nav>
+    <header-nav :title="title" 
+                :show="show" 
+                @changeShow="changeStateText"></header-nav>            
+                {{$data}}
+</div> 
+</template>
+<script>
+import HeaderNav from './components/Header.vue'
+export default {
+
+    data(){
+        return {
+            show: true,
+            title: "Titulo del padre"
+        }
+    },
+    methods: {
+        changeStateText(state){
+            this.show= state
+        }
+    },
+    computed: {
+
+    },
+    components:{
+        HeaderNav
+    }
+}
+</script>
+<style>
+
+</style>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1 v-if="show">{{title}}</h1>
+        <button @click="showOrHide"> Mostrar/Ocultar</button>
+        {{$props}}
+    </div>
+    
+</template>
+<script>
+export default {
+    data(){
+        return {
+            
+        }
+    },
+    props:{
+        show:{
+            type:Boolean,
+            required: true,
+            default: false
+        },
+        title: {
+            type: String,
+            required: true,
+            default: "No hay datos"}
+    },
+    methods:{
+        showOrHide(){
+            this.$emit("changeShow", !this.show)
+        }
+    }
+   
+}
+</script>
+
+
